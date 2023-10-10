21783ebf17777fe287da5a49beb7751e5367c130
katie <113192783+katiekhamarkhanova@users.noreply.github.com>
2023-10-10 09:31:24 -0400

    array

---
+++ objects.js
@@ -0,0 +1,23 @@
+const schoolBreak = [
+    {
+        monthName: "November",
+        daysOff: [23,24],
+        fullWeek: false
+    },
+    {
+        monthName: "December",
+        daysOff: [25,26,27,28,29,30,31,1],
+        fullWeek: true
+    },
+    {
+        monthName: "February",
+        daysOff: [19,20,21,22,23],
+        fullWeek: true
+    },
+    {
+        monthName: "April",
+        daysOff: [22,23,24,25,26,27,28,29,30],
+        fullWeek: true
+    }
+];
+console.log(schoolBreak);
