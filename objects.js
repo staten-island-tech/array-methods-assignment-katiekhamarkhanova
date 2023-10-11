790aacef365e3cfea44917e0df53338f70666469
katie <113192783+katiekhamarkhanova@users.noreply.github.com>
2023-10-11 09:26:20 -0400

    a

--- objects.js
+++ objects.js
@@ -1,23 +1,28 @@
 const schoolBreak = [
-    {
-        monthName: "November",
-        daysOff: [23,24],
-        fullWeek: false
-    },
-    {
-        monthName: "December",
-        daysOff: [25,26,27,28,29,30,31,1],
-        fullWeek: true
-    },
-    {
-        monthName: "February",
-        daysOff: [19,20,21,22,23],
-        fullWeek: true
-    },
-    {
-        monthName: "April",
-        daysOff: [22,23,24,25,26,27,28,29,30],
-        fullWeek: true
-    }
+  {
+    monthName: "November",
+    daysOff: [23, 24],
+    fullWeek: false,
+  },
+  {
+    monthName: "December",
+    daysOff: [25, 26, 27, 28, 29, 30, 31, 1],
+    fullWeek: true,
+  },
+  {
+    monthName: "February",
+    daysOff: [19, 20, 21, 22, 23],
+    fullWeek: true,
+  },
+  {
+    monthName: "April",
+    daysOff: [22, 23, 24, 25, 26, 27, 28, 29, 30],
+    fullWeek: true,
+  },
 ];
-console.log(schoolBreak);
+schoolBreak.forEach((schoolBreak) => console.log(schoolBreak.monthName));
+schoolBreak.forEach((schoolBreak) =>
+  console.log(schoolBreak.monthName, schoolBreak.daysOff, schoolBreak.fullWeek)
+);
+const week = schoolBreak.filter((schoolBreak) => schoolBreak.fullWeek === true);
+week.forEach((week) => console.log(week.monthName, week.fullWeek));
